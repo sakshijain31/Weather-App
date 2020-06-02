ea3d486e16a398180286918e99dc381a32f2ce7b
sakshi <sakshi.jain@simplilearn.net>
2020-06-02 17:44:44 +0530

    removing unnecessary code

--- public/js/app.js
+++ public/js/app.js
@@ -7,12 +7,11 @@ weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     
     msgOne.textContent = 'Loading.....'
-    fetch('http://localhost:3000/weather?address='+ search.value).then((resp) =>{
+    fetch('/weather?address='+ search.value).then((resp) =>{
         resp.json().then((data=>{
             if(data.error){
                 msgOne.textContent = data.error
             }
-            console.log(data)
             msgOne.textContent = data.location
             msgTwo.textContent = data.forecast
         }))
