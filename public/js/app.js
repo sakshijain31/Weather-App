8b033eff7a1b7893b72d81604999a4d737c35083
sakshi <sakshi.jain@simplilearn.net>
2020-06-02 17:20:45 +0530

    removing console

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side server is running')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const msgOne = document.querySelector('#msg-1')
